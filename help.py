c89cbd6080ca4d454441a11dc288ce79c91e27da
Danilo Nascimento <daconnas.dcn@gmail.com>
2021-11-23 15:24:57 -0300

    after format

--- help.py
+++ help.py
@@ -1,18 +1,19 @@
 from rich.console import Console
 
+
 console = Console()
 console.print("\nto run commands: [bold]make target[/bold]")
 
 
-with open("./Makefile", 'r') as file:
+with open("./Makefile", "r") as file:
     for line in file.readlines():
         if line.find("##") >= 0:
             if line.find("@") >= 0:
                 title = line.split("@")[-1].strip()
-                console.print(f'\n{title}', style="bold green")
+                console.print(f"\n{title}", style="bold green")
             else:
                 target, *values, description = line.split("##")
 
                 target = target.split(":")[0].strip()
                 description = description.strip().capitalize()
-                console.print(f'  - [bold]{target}[/bold]: {description}')
+                console.print(f"  - [bold]{target}[/bold]: {description}")
